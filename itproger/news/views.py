afd6c62ae2ef5cb12e975f1341899f2acee39482
Ilya Kuprin <lucifer39@bk.ru>
2024-05-16 13:44:05 +0200

    работа с jinja шаблонизатором, создание базы данных и страницы с сохранением вносимой информации

--- itproger/news/views.py
+++ itproger/news/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Articles
+from .forms import ArticlesForm
 # Create your views here.
 
 def news_home(requsts):
-    data = {
-        'title': "что то о новостях"
+    news = Articles.objects.order_by('-data_news')
+    return render(requsts, "news/news_home.html", {'news': news})
 
+def create(reqests):
+    error = ""
+    if reqests.method == 'POST':
+        form = ArticlesForm(reqests.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = "Форма заполенна не верно"
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form,
+        'error': error
     }
-    return render(requsts, "news/news_home.html", data)
+
+    return render(reqests, "news/create.html", data)
